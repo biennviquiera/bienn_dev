d05cb29682101a4492debe474c9ae66c2a67a5fe
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-16 19:38:11 -0700

    changed background color and body text color

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import './globals.css'
 
 export default function Home() {
   return (
     <>
-      <h1 className="font-bold text-4xl">Bienn Viquiera</h1>
+      <h1 className="font-bold text-4xl mb-4">Bienn Viquiera</h1>
       <div className="max-w-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     </>
   )
