d8dcd9b878cba5b59f6d596f9b1bb3a28fe885ba
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-16 22:12:44 -0700

    Added sidebar component, navigation between pages

---
+++ app/projects/page.js
@@ -0,0 +1,29 @@
+import Layout from '../layout'
+
+export default function Page() {
+    return (
+        <Layout>
+            <div className="max-w-sm">
+            Dropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+                Dropdowns of my projects will go here
+            </div>
+        </Layout>
+    );
+}
