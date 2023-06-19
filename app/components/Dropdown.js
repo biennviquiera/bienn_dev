52a824ff3c14d26a78ac3ed3e58e6da3a0ed1764
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-19 10:07:24 -0700

    Created dropdown component and created placeholder projects tabs

---
+++ app/components/Dropdown.js
@@ -0,0 +1,15 @@
+export default function Dropdown(props) {
+    return (
+        <details className="container group pb-8 transition-transform">
+            <summary className="active:text-green-400 transition-colors list-none flex flex-wrap items-center pb-3 cursor-pointer hover:text-neutral-300">
+                <h3 className="flex flex-1 font-semibold text-3xl">{props.name}</h3>
+                <h2 className="flex flex-start pr-8">{props.languages}</h2>
+                <div class="border-8 border-transparent border-l-white 
+                group-open:rotate-90 duration-300 transition-transform origin-left"></div>
+            </summary>
+            <p>
+                {props.children}
+            </p>
+        </details>
+    )
+}
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,28 +1,21 @@
 import Layout from '../layout'
+import Dropdown from '../components/Dropdown';
 
 export default function Page() {
     return (
         <Layout>
-            <div className="max-w-sm">
-            Dropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go hereDropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
-                Dropdowns of my projects will go here
+            <div className="container max-w-lg">
+                <Dropdown name="Disher" languages="Objective-C, Parse">
+                    Disher is an iOS application built with Objective-C that allows users to save their favorite dishes,
+                    view its ingredients, and search using ingredients they currently possess.
+                    
+                </Dropdown>
+
+                <Dropdown name="Yale IMs" languages="React Native, Flask, SQLite, AWS EC2">
+                    Yale Intramurals Sports is a mobile application for both iOS and React Native created to streamline the overall
+                    process of the intramurals process at Yale.
+
+                </Dropdown>
             </div>
         </Layout>
     );
