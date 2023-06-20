234548384a0ba10cbd949c0bd32b2d0507520207
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-19 17:06:38 -0700

    Last try at attempting to remove disclosure widget on mobile

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,7 +1,7 @@
 export default function Dropdown(props) {
     return (
         <details className="container group pb-8 transition-transform">
-            <summary className="active:text-green-400 transition-colors list-none flex flex-wrap items-center pb-3 cursor-pointer hover:text-neutral-300">
+            <summary style={{ listStyle: 'none'}} className="active:text-green-400 transition-colors list-none flex flex-wrap items-center pb-3 cursor-pointer hover:text-neutral-300">
                 <h3 className="flex flex-1 font-semibold text-3xl">{props.name}</h3>
                 <h2 className="flex flex-start pr-8">{props.languages}</h2>
                 <div className="border-8 border-transparent border-l-white 
