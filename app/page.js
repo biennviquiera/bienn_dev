e225d8caeb8c09532970ab769d5e2911de145fb8
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-23 15:52:24 -0700

    Added GitHub and LinkedIn Logos + Links to about page, underline email to link to email

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 'use client'
 
 import Image from 'next/image'
+import Link from 'next/link'
 import Layout from './layout'
 import './globals.css'
 import { about } from '../lib/text'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
@@ -11,6 +13,32 @@ export default function Home() {
         <h1 className="font-bold text-4xl mb-4">Bienn Viquiera</h1>
         <div>
           {about()}
+
+          <section className="container flex gap-6 items-center">
+            {/* GitHub Logo */}
+            <Link href="https://github.com/biennviquiera" target="_blank">
+              <Image 
+              src="/github-mark-white.svg" 
+              target="_blank"
+              alt="Bienn's GitHub Profile"
+              width={50}
+              height={100}
+              />
+            </Link>
+            
+            {/* LinkedIn Logo */}
+            <Link href="https://linkedin.com/in/bienn-viquiera" target="_blank">
+              <Image 
+              src="/linkedin-white.png"
+              alt="Bienn's LinkedIn Page"
+              width={50}
+              height={100}
+              stroke="white"
+              />
+            </Link>
+
+          </section>
+
         </div>
       </Layout>
   )
