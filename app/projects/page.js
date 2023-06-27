24b302b294b40e8eece6cc4b128791bc8a9741df
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-27 06:05:05 -0700

    Created card-like styling for dropdown component

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,38 +2,36 @@ import Layout from '../layout'
 import Dropdown from '../../components/Dropdown';
 
 export default function Page() {
-    return (
-        <div className="flex flex-row md:flex-col justify-center relative">
-            <Layout>
-                <div className="container">
-                    <Dropdown name="Disher" languages="Objective-C, Parse">
-                        <p>
-                            Disher is an iOS application built with Objective-C that allows users to save their favorite dishes, 
-                            view its ingredients, and search using ingredients they currently possess.
-                        </p>
-                    </Dropdown>
+	return (
+		<Layout>
+			<div className="container">
+				<Dropdown name="Disher" languages="Objective-C, Parse">
+					<p>
+						Disher is an iOS application built with Objective-C that allows users to save their favorite dishes,
+						view its ingredients, and search using ingredients they currently possess.
+					</p>
+				</Dropdown>
 
-                    <Dropdown name="Yale IMs" languages="React Native, Flask, SQLite, AWS EC2">
-                        <p>
-                            Yale Intramurals Sports is a mobile application for both iOS and React Native created to streamline the overall
-                            process of the intramurals process at Yale.
-                        </p>
-                    </Dropdown>
+				<Dropdown name="Yale IMs" languages="React Native, Flask, SQLite, AWS EC2">
+					<p>
+						Yale Intramurals Sports is a mobile application for both iOS and React Native created to streamline the overall
+						process of the intramurals process at Yale.
+					</p>
+				</Dropdown>
 
-                    <Dropdown name="MP3 Looper" languages="Next.js, Express.js, AWS S3">
-                        <p>
-                            MP3 Looper is a web application that users to upload mp3 files and select a snippet to loop. 
-                            It was inspired by a loop I created which received 800,000+ views on YouTube
-                        </p>
-                    </Dropdown>
+				<Dropdown name="MP3 Looper" languages="Next.js, Express.js, AWS S3">
+					<p>
+						MP3 Looper is a web application that users to upload mp3 files and select a snippet to loop.
+						It was inspired by a loop I created which received 800,000+ views on YouTube
+					</p>
+				</Dropdown>
 
-                    <Dropdown name="SkipPlayer" languages="Python, PyQt5, ffpmeg">
-                        <p>
-                            SkipPlayer is a Python program aimed to help students review lectures by skipping silent portions of recordings.
-                        </p>
-                    </Dropdown>
-                </div>
-            </Layout>
-        </div>
-    );
+				<Dropdown name="SkipPlayer" languages="Python, PyQt5, ffpmeg">
+					<p>
+						SkipPlayer is a Python program aimed to help students review lectures by skipping silent portions of recordings.
+					</p>
+				</Dropdown>
+			</div>
+		</Layout>
+	);
 }
--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,13 +1,13 @@
 export default function Dropdown(props) {
     return (
-        <details className="container group pb-8 transition-transform">
-            <summary className="active:text-green-400 transition-colors flex flex-wrap items-center pb-3 cursor-pointer hover:text-neutral-300">
+        <details className="container group mb-8 transition-transform rounded border-white-400 border-l border-t border-b border-r">
+            <summary className="m-8 active:text-green-400 transition-colors flex flex-wrap items-center pb-3 cursor-pointer hover:text-neutral-300">
                 <h3 className="flex flex-1 font-semibold text-3xl">{props.name}</h3>
                 <h2 className="flex flex-start pr-8">{props.languages}</h2>
-                {/* <div className="border-8 border-transparent border-l-white 
-                group-open:rotate-90 duration-300 transition-transform origin-left"></div> */}
+                <div className="border-8 border-transparent border-l-white 
+                group-open:rotate-90 duration-300 transition-transform origin-left"></div>
             </summary>
-            {props.children}
+            <div className="m-8">{props.children}</div>
         </details>
     )
 }
